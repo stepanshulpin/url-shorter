8b48800ba4abcd742c0547b38bd014f9b1e6b9c4
stsh0318 <stepan.shulpin@netcracker.com>
2022-08-29 01:17:14 +0300

    local starter

---
+++ src/test/java/com/stsh/urlshorter/StartApplication.java
@@ -0,0 +1,11 @@
+package com.stsh.urlshorter;
+
+import org.springframework.boot.SpringApplication;
+
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShorterApplication.class, args);
+    }
+
+}
